626a4d51b361326aa23bd857e872392b26494341
dat <dat030602@gmail.com>
2022-06-09 10:55:07 +0700

    babel-plugin-module-resolver

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
+import Button from '~/components/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,5 +23,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
